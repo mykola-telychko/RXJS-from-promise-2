95fd965fb8c6fb99d91e00499aeb56b2c232cf1b
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 20:57:14 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,35 +1,4 @@
 import { from } from 'rxjs';
-import { http } from 'http';
-
-function httpRequest(url) {
-  return new Promise((resolve, reject) => {
-    http.get(url, (response) => {
-      let data = '';
-
-      response.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      response.on('end', () => {
-        resolve(data);
-      });
-
-      response.on('error', (error) => {
-        reject(error);
-      });
-    });
-  });
-}
-
-const url = 'http://jsonplaceholder.typicode.com/posts/1'; // Example URL
-
-httpRequest(url)
-  .then((responseData) => {
-    console.log('Response:', responseData);
-  })
-  .catch((error) => {
-    console.error('Error:', error.message);
-  });
 
 // https://www.learnrxjs.io/learn-rxjs/operators/creation/from
 // Example 2: Observable from promise
