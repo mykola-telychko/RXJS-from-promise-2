85c0d3324d091d024ae06eed700dca41fbc39814
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 20:38:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-from-promise-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-wtstyv)
---
+++ index.ts
@@ -0,0 +1,37 @@
+import { from } from 'rxjs';
+import http from 'http';
+
+function httpRequest(url) {
+  return new Promise((resolve, reject) => {
+    http.get(url, (response) => {
+      let data = '';
+
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        resolve(data);
+      });
+
+      response.on('error', (error) => {
+        reject(error);
+      });
+    });
+  });
+}
+
+const url = 'http://jsonplaceholder.typicode.com/posts/1'; // Example URL
+
+httpRequest(url)
+  .then((responseData) => {
+    console.log('Response:', responseData);
+  })
+  .catch((error) => {
+    console.error('Error:', error.message);
+  });
+
+//emit result of promise
+const promiseSrc = from(new Promise((resolve) => resolve('Hello World!')));
+//output: 'Hello World'
+const subscribe = promiseSrc.subscribe((val) => console.log(val));
